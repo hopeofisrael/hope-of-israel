193b72f2d5cd24e2cc3af47ab7eaaa0e48b573ef
ccann4805 <womanwhoserves@outlook.com>
2024-12-03 22:45:41 -0700

    Fix Google Sheets integration

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/(.*)', // This applies to all pages
+        source: '/(.*)', // This applies to all routes
         headers: [
           {
             key: 'Content-Security-Policy',
@@ -15,13 +15,15 @@ module.exports = {
               font-src 'self' https://fonts.gstatic.com;
               connect-src 'self' https://vercel.live https://www.googleapis.com https://firestore.googleapis.com https://firebase.googleapis.com https://identitytoolkit.googleapis.com ${process.env.NEXT_PUBLIC_PROXY_URL};
               img-src 'self' data:;
-            `.replace(/\n/g, ' ').replace(/\s{2,}/g, ' ').trim(), // Normalize spaces and line breaks
+              frame-src 'self' https://vercel.live; 
+            `.replace(/\n/g, ' ').replace(/\s{2,}/g, ' ').trim(), // Minify policy string
           },
         ],
       },
     ];
   },
   env: {
+    // Environment variables for use in your application
     NEXT_PUBLIC_PROXY_URL: process.env.NEXT_PUBLIC_PROXY_URL,
     API_URL: process.env.API_URL,
   },
