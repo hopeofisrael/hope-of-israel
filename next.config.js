e64f1d6b240243f1a1b2e3aebc87b291b0e04528
ccann4805 <womanwhoserves@outlook.com>
2024-12-05 05:44:07 -0700

    Updated CSP to allow Vercel scripts and removed unsafe-eval

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' 
+              script-src 'self' 
                 https://www.gstatic.com https://www.googleapis.com 
                 https://securetoken.googleapis.com https://identitytoolkit.googleapis.com 
                 https://script.google.com https://vercel.live;
